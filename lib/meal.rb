377da7a30281127ae77775b0fe0ae289bd2eb1fd
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-24 22:55:08 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,11 +1,13 @@
 class Meal
   attr_accessor :waiter, :customer, :total, :tip
   
+  @@all = []
+  
   def initialize(waiter, customer, total, tip= 0)
     @waiter = waiter
     @customer = customer
     @total = total
-    @tip = 0
+    @tip = tip
     @@all << self
   end
   
